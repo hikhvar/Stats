36fdc90485bd7d13baddc9022dfcbc50a020baa1
hikhvar <chr.robbert@web.de>
2015-01-03 10:13:34 +0100

    Added function to return main directory on windows.

--- client/setup.py
+++ client/setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 import py2exe
 from glob import glob
-import numpy
+#import numpy
 import sys
 import matplotlib
 #import scipy.stats
 opts={"py2exe":{"bundle_files"}}
 sys.argv.append('py2exe')
 opts = {'py2exe': 
-	{"bundle_files" : 3, 
+	{"bundle_files" : 3,
 	"includes" : [ "matplotlib.backends", "pylab","numpy"],#, 'scipy.sparse.csgraph._validation', 'scipy.special._ufuncs_cxx'], 
 	'excludes':['_gtkagg', '_tkagg', '_agg2', '_cairo', '_cocoaagg', '_fltkagg', '_gtk', '_gtkcairo', 'scipy'], 
 	'dll_excludes': ['libgdk-win32-2.0-0.dll', 'libgobject-2.0-0.dll']}}
